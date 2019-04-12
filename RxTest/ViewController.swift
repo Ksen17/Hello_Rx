144bdb4d2fec9ff9d140125f5c12aeacac2cf085
Ilia Stukalov <iStukalov96@yandex.ru>
2019-04-12 16:12:34 +0300

    Initial Commit

---
+++ RxTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxTest
+//
+//  Created by Ilia on 12/04/2019.
+//  Copyright © 2019 Ilia Stukalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
